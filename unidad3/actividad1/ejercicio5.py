bb4f20eef81e82e39ae6c33b2eeedc40b662a6df
Poloche <sebastianpolochetellez@gmail.com>
2025-11-09 20:59:44 -0500

    ejercicios terminados

--- unidad3/actividad1/ejercicio5.py
+++ unidad3/actividad1/ejercicio5.py
@@ -9,26 +9,26 @@ import threading
 import time
 
 class Logger_daemon(threading.Thread):
-    def _init_(self, intervalo):
-        super()._init_(daemon=True)  
+    def __init__(self, intervalo):
+        super().__init__(daemon=True)  
         self.intervalo = intervalo
 
     def run(self):
         while True:
-            print(" El programa sigue ejecutándose...")
+            print("El programa sigue ejecutándose...")
             time.sleep(self.intervalo)
 
 
 class Trabajo_pesado(threading.Thread):
-    def _init_(self, pasos):
-        super()._init_()
+    def __init__(self, pasos):
+        super().__init__()
         self.pasos = pasos
 
     def run(self):
         for i in range(1, self.pasos + 1):
-            print(f" Ejecutando paso {i}/{self.pasos}...")
+            print(f"Ejecutando paso {i}/{self.pasos}...")
             time.sleep(1.2)  
-        print("trabajo pesado a terminado.")
+        print("Trabajo pesado a terminado.")
 
 
 def main():
@@ -39,11 +39,10 @@ def main():
     logger.start()
     trabajo.start()
 
-    
     trabajo.join()
 
-    print("el programa principal a terminado.")
+    print("El programa principal a terminado.")
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
